b79b5657f0ccbd9e99872a5c734ad439d86d8ac1
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-07-08 17:50:29 -0500

    router, About

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+const About = (props) => {
+    return(
+      <div className="about">
+        <h1><span>i</span>Stocks</h1>
+        <p>A simple app to display stock info</p>
+      </div>
+    ) 
+  };
+  export default About;
