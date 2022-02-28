ae0b7ebf1d4eff90b63976018b0841f91d9f6f55
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-02-28 11:28:56 -0600

    environment variables

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -11,8 +11,8 @@ const Stock = (props) => {
 
   const getStock=async()=>{
     //log stock url for dev purposes 
-    console.log("url: "+ process.env.REACT_APP_STOCK_URL+props.match.params.symbol)
-    const response = await fetch(process.env.REACT_APP_STOCK_URL+props.match.params.symbol)
+    console.log("url: "+ REACT_APP_STOCK_URL+props.match.params.symbol)
+    const response = await fetch(REACT_APP_STOCK_URL+props.match.params.symbol)
     const data= await response.json()
     // log data to check result of api call 
     console.log("call result: " + data)
